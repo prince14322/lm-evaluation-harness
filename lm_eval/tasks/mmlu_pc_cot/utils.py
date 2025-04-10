597d365aea446f1dc77b6266810fc37230c36b2f
Prince Kumar <prince.kumar12@ibm.com>
2025-04-10 13:56:58 -0400

    added mmul pc cot

---
+++ lm_eval/tasks/mmlu_pc_cot/utils.py
@@ -0,0 +1,34 @@
+def clean_pc_cot(text):
+    if "[/PSEUDOCODE]" in text:
+        return text.split("[/PSEUDOCODE]")[-1].strip()
+
+    if "[/[PSEUDOCODE]]" in text:
+        return  text.split("[/[PSEUDOCODE]]")[-1].strip()
+    
+    pattern = re.compile(r'>>>\s*\w+\s*\([^)]*\)(.*)', re.DOTALL)
+    match = pattern.search(text)
+    if match:
+        return match.group(1).strip()
+
+    return ""
+
+def process_results_gen(doc, results):
+    gold = doc["answerKey"].lower()
+
+    candidate = results[0]
+
+    candidate = clean_pc_cot(candidate)
+
+    candidate = candidate.strip().lower().split("\n")[0].split(" ")[0].strip()
+
+    if "." in candidate:
+        candidate = candidate.split(".")[0]
+
+    retval = 0
+    if candidate == gold:
+        retval = 1
+    
+    results = {
+        "exact_match": retval,
+    }
+    return results
